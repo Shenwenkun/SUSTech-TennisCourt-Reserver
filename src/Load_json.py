713397c1a9330fe481defd883d47a11e2c2d9181
Shenwenkun <12211717@mail.sustech.edu.cn>
2025-09-23 18:19:27 -0700

    can reserve tennis court now

--- src/Load_json.py
+++ src/Load_json.py
@@ -3,7 +3,7 @@ import json
 
 
 def get_config():
-    with open("config.json", "r", encoding='utf-8') as f:
+    with open("../config.json", "r", encoding='utf-8') as f:
         text = json.loads(f.read())
         text["order_time"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         return text
--- src/main.py
+++ src/main.py
@@ -8,25 +8,25 @@ def reserve(config, start_time, end_time, ground_index):
           + "&token=" + config["token"]
     captchaVerification = str(Verification())[2:-1]  # The original value seems like b'xxxxxxxx', so I use [2:-1]
     data = {
-        "customerName": config["student_name"],
-        "customerId": "1433554008613634050",
-        "customerTel": config["student_tel"],
-        "userNum": 1,
+        "captchaVerification": captchaVerification,
         "customerEmail": "",
-        "gymId": "1297443858304540673",
-        "gymName": "润杨羽毛球馆",
+        "customerId": "1596483041392003586",
+        "customerName": config["student_name"],
+        "customerTel": config["student_tel"],
+        "endTime": end_time,
         "groundId": config["ground_url"][str(ground_index)],
         "groundName": config["ground_name"][str(ground_index)],
         "groundType": "0",
-        "messagePushType": "0",
+        "gymId": "1297442093383360513",
+        "gymName": "欣园网球场",
         "isIllegal": "0",
+        "messagePushType": "0",
         "orderDate": config["order_time"],
         "startTime": start_time,
-        "endTime": end_time,
+        "tmpEndTime": end_time,
         "tmpOrderDate": config["order_time"],
         "tmpStartTime": start_time,
-        "tmpEndTime": end_time,
-        "captchaVerification": captchaVerification
+        "userNum": 1
     }
     headers = config["headers"]
     try:
@@ -36,7 +36,7 @@ def reserve(config, start_time, end_time, ground_index):
             print("You have reserved Ground " + str(ground_index) + " from " + start_time + " to " + end_time)
             return True
         else:
-            print(f"Failed to reserve Ground {ground_index} from {start_time} to {end_time} --> {re.text}")
+            print(f"Failed to reserve Ground {str(ground_index)} from {start_time} to {end_time}--> {re.text}")
             return False
     except requests.RequestException as e:
         print(f"{e}")
@@ -45,15 +45,18 @@ def reserve(config, start_time, end_time, ground_index):
 
 if __name__ == "__main__":
     config = get_config()
-    for start_time, end_time in zip(config["start_time"], config["end_time"]):
-        for ground_id in list(config["ground_url"].keys()):
-            if ground_id == "3" or ground_id == "4" or ground_id == "8" or ground_id == "10":
-                continue
-            print(f"Reserve Ground {ground_id} from {start_time} to {end_time}")
-            result = False
-            try:
-                result = reserve(config, start_time, end_time, ground_id)
-            except Exception as e:
-                pass
-            if result:
-                break
+    flag=0
+    while flag!=1:
+        for start_time, end_time in zip(config["start_time"], config["end_time"]):
+            for ground_id in list(config["ground_url"].keys()):
+                if ground_id == "2" or ground_id == "7":
+                    continue
+                # print(f"Reserve Ground {ground_id} from {start_time} to {end_time}")
+                result = False
+                try:
+                    result = reserve(config, start_time, end_time, ground_id)
+                except Exception as e:
+                    pass
+                if result:
+                    flag=1
+                    break
